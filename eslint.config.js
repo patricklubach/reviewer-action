5fabef928e6434bf74ea947783f7df167c6a38f6
Patrick Lubach <plubach1994@gmail.com>
2024-07-05 20:30:04 +0200

    Fix: Pipeline super-linter failure (#8)
    * fix: action.yml runs.main reference
    
    * feat: Run linter action on push to every branch
    
    * fix: ci.yml github action
    
    * chore: Add ESlint to pipeline
    
    * feat: Add prettier as dev dependendency
    
    * fix: Formatter step
    
    * Update package.json
    
    * fix: Remove super-linter

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,13 @@ import js from '@eslint/js'
 export default [
   js.configs.recommended,
   {
+    ignores: [
+      '__tests__/*',
+      '.devcontainer/*',
+      '.github/*',
+      'badges/*',
+      'dist/*'
+    ],
     rules: {
       'no-console': 'off',
       'no-unused-vars': 'warn',
