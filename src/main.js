26aea920e4166706aca0ebc36ea48783cee7355a
Patrick Lubach <plubach1994@gmail.com>
2024-12-14 20:40:33 +0100

    fix: Rule was not checked when no reviews (#33)
    * fix: Rule was not checked when no reviews
    
    * fix: Cannot acces pullRequest object before init

--- src/main.js
+++ src/main.js
@@ -16,26 +16,10 @@ async function run() {
     core.debug(`repo_name: ${repo_name}`)
     core.debug(`pr_number: ${pr_number}`)
 
-    // Get a list of all reviews of the PR
-    const { data: reviews } = await utils.getReviews(
-      octokit,
-      owner,
-      repo_name,
-      pr_number
-    )
-    core.debug(reviews)
-    if(reviews.length == 0) {
-      core.info('There are no reviews to check')
-      return
-    } else {
-      core.info(`There are ${reviews.length} reviews to check`)
-    }
 
-    // Filter reviews by status == 'APPROVED'
-    const approvedReviews = utils.getApprovals(reviews)
-
-    // Filter reviews by users who already reviewed and approved the PR
-    const approvers = utils.getApprovers(approvedReviews)
+    // Get the data from config file
+    const filePath = core.getInput('approvers_file', { required: false })
+    const approverFile = utils.getYamlData(filePath)
 
     // Get the pull request
     const { data: pullRequest } = await utils.getPullRequest(
@@ -49,12 +33,28 @@ async function run() {
     const pullRequestTitle = pullRequest.title
     core.debug(`Pull request title is "${pullRequestTitle}"`)
 
-    // Get the data from config file
-    const filePath = core.getInput('approvers_file', { required: false })
-    const approverFile = utils.getYamlData(filePath)
-
     // Get the rule who matches the PR title
     const rule = utils.getMatchingRule(pullRequestTitle, approverFile)
+
+    // Get a list of all reviews of the PR
+    const { data: reviews } = await utils.getReviews(
+      octokit,
+      owner,
+      repo_name,
+      pr_number
+    )
+    core.debug(reviews)
+    if(reviews.length == 0) {
+      core.info('There are no reviews to check')
+    } else {
+      core.info(`There are ${reviews.length} reviews to check`)
+    }
+
+    // Filter reviews by status == 'APPROVED'
+    const approvedReviews = utils.getApprovals(reviews)
+
+    // Filter reviews by users who already reviewed and approved the PR
+    const approvers = utils.getApprovers(approvedReviews)
 
     // Get the list of all desired approvers
     const desiredApprovers = utils.computeApprovers(
