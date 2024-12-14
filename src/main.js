9ed2062d9c1f5ca460fa144492f289673f11b569
patricklubach <plubach1994@gmail.com>
2024-12-14 19:10:06 +0100

    fix: Repository name

--- src/main.js
+++ src/main.js
@@ -10,17 +10,17 @@ import * as utils from './utils.js'
 
 async function run() {
   try {
-    const repo = process.env.GITHUB_ACTION_REPOSITORY
+    const repo = process.env.GITHUB_REPOSITORY
     const repo_name = repo.split('/')[1]
     const owner = process.env.GITHUB_REPOSITORY_OWNER
     const pr_number = core.getInput('pr_number', { required: true })
     const token = core.getInput('gh_token', { required: true })
     const octokit = github.getOctokit(token)
 
-    octokit.log.info(`repo: ${repo}`)
-    octokit.log.info(`repo_name: ${repo_name}`)
-    octokit.log.info(`owner: ${owner}`)
-    octokit.log.info(`pr_number: ${pr_number}`)
+    console.log(`repo: ${repo}`)
+    console.log(`repo_name: ${repo_name}`)
+    console.log(`owner: ${owner}`)
+    console.log(`pr_number: ${pr_number}`)
 
     // Get a list of all reviews of the PR
     const reviews = utils.getReviews(octokit, owner, repo_name, pr_number)
@@ -62,7 +62,7 @@ async function run() {
   } catch(error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error)
-    return
+    throw error;
   }
 }
 
